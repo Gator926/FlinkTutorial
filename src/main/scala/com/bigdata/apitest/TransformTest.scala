190cc2d11737eefc705ce4b5aebf2a58e5abf4e9
gator <zhangpei529@sohu.com>
2021-08-03 17:06:14 +0800

    commit

--- src/main/scala/com/bigdata/apitest/TransformTest.scala
+++ src/main/scala/com/bigdata/apitest/TransformTest.scala
@@ -1,5 +1,6 @@
 package com.bigdata.apitest
 
+import org.apache.flink.api.common.functions.ReduceFunction
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.scala._
 
@@ -14,8 +15,23 @@ object TransformTest {
       val arr = data.split(",")
       SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
     })
+
+    // 分组聚合, 输出每个传感器当前最小值
     val aggStream = dataStream.keyBy(_.id).minBy("temperature")
-    aggStream.print()
+//    aggStream.print()
+
+    // 需要输出当前最小的温度值, 以及最近的时间戳, 要用reduce
+    val resultStream = dataStream.keyBy(_.id).reduce((curState, newData) => {
+      SensorReading(curState.id, newData.timestamp, curState.temperature.min(newData.temperature))
+    })
+//    resultStream.print()
+
+    val resultStream1 = dataStream.keyBy(_.id).reduce(new ReduceFunction[SensorReading] {
+      override def reduce(t: SensorReading, t1: SensorReading): SensorReading = {
+        SensorReading(t.id, t1.timestamp, t.temperature.min(t1.temperature))
+      }
+    })
+    resultStream1.print()
 
     environment.execute(this.getClass.getSimpleName)
   }
