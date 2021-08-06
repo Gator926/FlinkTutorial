e31c2b680ff31b722662fc2b9e8f321a592ce2a9
gator <zhangpei529@sohu.com>
2021-08-06 10:04:04 +0800

    commit

--- src/main/scala/com/bigdata/apitest/WindowTest.scala
+++ src/main/scala/com/bigdata/apitest/WindowTest.scala
@@ -1,7 +1,25 @@
 package com.bigdata.apitest
 
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.scala._
+
 object WindowTest {
   def main(args: Array[String]): Unit = {
+    val environment = StreamExecutionEnvironment.getExecutionEnvironment
+    // 时间语义
+    environment.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
+    val inputPath = "src/main/resources/sensor.txt"
+    val stream = environment.readTextFile(inputPath)
+
+    val dataStream = stream.map(data => {
+      val arr = data.split(",")
+      SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
+    })
+
+
+
+    environment.execute()
   }
 }
