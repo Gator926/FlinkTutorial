8faf44b1109ca0d56c56d0a1ac3993da3035daab
gator <zhangpei529@sohu.com>
2021-08-09 09:36:48 +0800

    commit

--- src/main/scala/com/bigdata/apitest/WindowTest.scala
+++ src/main/scala/com/bigdata/apitest/WindowTest.scala
@@ -1,8 +1,10 @@
 package com.bigdata.apitest
 
 import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
 
 object WindowTest {
   def main(args: Array[String]): Unit = {
@@ -17,8 +19,12 @@ object WindowTest {
       val arr = data.split(",")
       SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
     })
-
-
+//      .assignAscendingTimestamps(_.timestamp * 1000L)   // 升序数据提取时间戳
+      .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[SensorReading](Time.seconds(3)) {
+        override def extractTimestamp(element: SensorReading): Long = {
+          element.timestamp * 1000L
+        }
+      })
 
     environment.execute()
   }
