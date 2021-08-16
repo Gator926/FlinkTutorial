b8a533dc7e5f488baa016bb50edd165153554104
gator <zhangpei529@sohu.com>
2021-08-16 16:29:24 +0800

    commit

--- src/main/scala/com/bigdata/tabletest/TableAPITest.scala
+++ src/main/scala/com/bigdata/tabletest/TableAPITest.scala
@@ -4,7 +4,7 @@ import org.apache.flink.api.scala.ExecutionEnvironment
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.table.api.{DataTypes, EnvironmentSettings, Table, TableEnvironment}
 import org.apache.flink.table.api.scala.{BatchTableEnvironment, StreamTableEnvironment}
-import org.apache.flink.table.descriptors.{Csv, FileSystem, OldCsv, Schema}
+import org.apache.flink.table.descriptors.{Csv, FileSystem, Kafka, OldCsv, Schema}
 import org.apache.flink.table.api.scala._
 import org.apache.flink.streaming.api.scala._
 
@@ -29,20 +29,36 @@ object TableAPITest {
 //    val blinkBatchSettings = EnvironmentSettings.newInstance().useBlinkPlanner().inBatchMode().build()
 //    val blinkBatchTableEnv = TableEnvironment.create(blinkBatchSettings)
 
-    // 2. 连接外部系统, 读取数据, 注册表
+    // 2.1 连接外部系统, 读取数据, 注册表
     val filePath = "src/main/resources/sensor.txt"
+
+    val schema: Schema = new Schema()
+      .field("id", DataTypes.STRING())
+      .field("timestamp", DataTypes.BIGINT())
+      .field("temperature", DataTypes.DOUBLE())
+
     tableEnv.connect(new FileSystem().path(filePath))
       .withFormat(new Csv)
       .withSchema(
-        new Schema()
-          .field("id", DataTypes.STRING())
-          .field("timestamp", DataTypes.BIGINT())
-          .field("temperature", DataTypes.DOUBLE())
+        schema
       )
       .createTemporaryTable("inputTable")
+//    val inputTable: Table = tableEnv.from("inputTable")
+//    inputTable.toAppendStream[(String, Long, Double)].print("inputTable")
 
-    val inputTable: Table = tableEnv.from("inputTable")
-    inputTable.toAppendStream[(String, Long, Double)].print("inputTable")
+    // 2.2 从kafka读取数据
+    tableEnv.connect(new Kafka()
+    .version("0.11")
+    .topic("sensor")
+    .property("zookeeper.connect", "dev41:2181,dev42:2181,dev43:2181")
+    .property("bootstrap.servers", "dev41:9092,dev42:9092,dev43:9092")
+    )
+      .withFormat(new Csv)
+      .withSchema(
+        schema
+      ).createTemporaryTable("kafkaInputTable")
+      val kafkaInputTable: Table = tableEnv.from("kafkaInputTable")
+    kafkaInputTable.toAppendStream[(String, Long, Double)].print("kafkaInputTable")
 
 
 
