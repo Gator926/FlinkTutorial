4d927c996dc6657d3ca3c1e92c641a8be40e01e4
gator <zhangpei529@sohu.com>
2021-08-16 16:11:59 +0800

    commit

--- src/main/scala/com/bigdata/tabletest/TableAPITest.scala
+++ src/main/scala/com/bigdata/tabletest/TableAPITest.scala
@@ -2,8 +2,11 @@ package com.bigdata.tabletest
 
 import org.apache.flink.api.scala.ExecutionEnvironment
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-import org.apache.flink.table.api.{EnvironmentSettings, TableEnvironment}
+import org.apache.flink.table.api.{DataTypes, EnvironmentSettings, Table, TableEnvironment}
 import org.apache.flink.table.api.scala.{BatchTableEnvironment, StreamTableEnvironment}
+import org.apache.flink.table.descriptors.{FileSystem, OldCsv, Schema}
+import org.apache.flink.table.api.scala._
+import org.apache.flink.streaming.api.scala._
 
 object TableAPITest {
   def main(args: Array[String]): Unit = {
@@ -15,16 +18,31 @@ object TableAPITest {
     val oldStreamTableEnv = StreamTableEnvironment.create(env, settings)
 
     // 1.2 基于老版本的批处理
-    val batchEnv = ExecutionEnvironment.getExecutionEnvironment
-    val oldBatchTableEnv = BatchTableEnvironment.create(batchEnv)
+//    val batchEnv = ExecutionEnvironment.getExecutionEnvironment
+//    val oldBatchTableEnv = BatchTableEnvironment.create(batchEnv)
 
     // 1.3 基于blink planner的流处理
-    val blinkStreamSettings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build()
-    val blinkStreamTableEnv = StreamTableEnvironment.create(env, blinkStreamSettings)
+//    val blinkStreamSettings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build()
+//    val blinkStreamTableEnv = StreamTableEnvironment.create(env, blinkStreamSettings)
 
     // 1.4 基于blink planner的批处理
-    val blinkBatchSettings = EnvironmentSettings.newInstance().useBlinkPlanner().inBatchMode().build()
-    val blinkBatchTableEnv = TableEnvironment.create(blinkBatchSettings)
+//    val blinkBatchSettings = EnvironmentSettings.newInstance().useBlinkPlanner().inBatchMode().build()
+//    val blinkBatchTableEnv = TableEnvironment.create(blinkBatchSettings)
+
+    // 2. 连接外部系统, 读取数据, 注册表
+    val filePath = "src/main/resources/sensor.txt"
+    tableEnv.connect(new FileSystem().path(filePath))
+      .withFormat(new OldCsv)
+      .withSchema(
+        new Schema()
+          .field("id", DataTypes.STRING())
+          .field("timestamp", DataTypes.BIGINT())
+          .field("temperature", DataTypes.DOUBLE())
+      )
+      .createTemporaryTable("inputTable")
+
+    val inputTable: Table = tableEnv.from("inputTable")
+    inputTable.toAppendStream[(String, Long, Double)].print("inputTable")
 
     env.execute()
   }
