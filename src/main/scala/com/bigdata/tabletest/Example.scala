8cae6b5c64cd3cce7df22cae1fb672d227881752
gator <zhangpei529@sohu.com>
2021-08-16 14:25:05 +0800

    commit

--- src/main/scala/com/bigdata/tabletest/Example.scala
+++ src/main/scala/com/bigdata/tabletest/Example.scala
@@ -3,10 +3,12 @@ package com.bigdata.tabletest
 import com.bigdata.apitest.SensorReading
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.table.api.scala.StreamTableEnvironment
+import org.apache.flink.table.api.scala._
 
 object Example {
   def main(args: Array[String]): Unit = {
-    val environment = StreamExecutionEnvironment.getExecutionEnvironment
+    val environment: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
 
     val inputPath = "src/main/resources/sensor.txt"
     val inputStream = environment.readTextFile(inputPath)
@@ -16,7 +18,23 @@ object Example {
       SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
     })
 
-    dataStream.print()
+    // 创建表执行环境
+    val tableEnv = StreamTableEnvironment.create(environment)
+
+    // 基于流创建一张表
+    val table = tableEnv.fromDataStream(dataStream)
+
+    // api转换
+    table.select("id, temperature")
+      .filter("id == 'sensor_1'")
+      .toAppendStream[(String, Double)]
+      .print("result")
+
+    // sql
+    tableEnv.createTemporaryView("dataTable", table)
+    tableEnv.sqlQuery("select id, temperature from dataTable where id = 'sensor_6'")
+      .toAppendStream[(String, Double)]
+      .print("sql")
 
     environment.execute()
   }
