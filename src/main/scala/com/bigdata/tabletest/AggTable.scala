6591f4c38f5490eadff74a952d5fe41c629bfbf0
gator <zhangpei529@sohu.com>
2021-08-19 10:53:29 +0800

    commit

---
+++ src/main/scala/com/bigdata/tabletest/AggTable.scala
@@ -0,0 +1,30 @@
+package com.bigdata.tabletest
+
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.table.api.DataTypes
+import org.apache.flink.table.api.scala._
+import org.apache.flink.table.descriptors.{Csv, FileSystem, Schema}
+
+object AggTable {
+  def main(args: Array[String]): Unit = {
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    val tableEnv = StreamTableEnvironment.create(env)
+
+    val schema = new Schema()
+      .field("id", DataTypes.STRING())
+      .field("timestamps", DataTypes.BIGINT())
+      .field("temperature", DataTypes.DOUBLE())
+
+    tableEnv.connect(new FileSystem().path("src/main/resources/sensor.txt"))
+      .withFormat(new Csv)
+      .withSchema(schema)
+      .createTemporaryTable("sensorTable")
+
+    tableEnv.from("sensorTable")
+      .groupBy('id)
+      .select('id, 'temperature.avg as 'temperature)
+      .toRetractStream[(String, Double)].print()
+
+    env.execute()
+  }
+}
---
+++ src/main/scala/com/bigdata/tabletest/kafkaOutputTest.scala
@@ -0,0 +1,52 @@
+package com.bigdata.tabletest
+
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.table.api.DataTypes
+import org.apache.flink.table.api.scala._
+import org.apache.flink.table.descriptors.{Csv, FileSystem, Kafka, Schema}
+
+object kafkaOutputTest {
+  def main(args: Array[String]): Unit = {
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    val tableEnv = StreamTableEnvironment.create(env)
+    env.setParallelism(1)
+
+    val schema = new Schema()
+      .field("id", DataTypes.STRING())
+      .field("timestamps", DataTypes.BIGINT())
+      .field("temperature", DataTypes.DOUBLE())
+
+    tableEnv.connect(new FileSystem().path("src/main/resources/sensor.txt"))
+      .withFormat(new Csv)
+      .withSchema(schema)
+      .createTemporaryTable("sensorTable")
+
+    val filterTable = tableEnv.from("sensorTable")
+      .select('id, 'temperature)
+      .filter('id === "sensor_1")
+
+    val aggTable = tableEnv.from("sensorTable")
+      .groupBy('id)
+      .select('id, 'temperature.sum as 'temperature)
+
+    tableEnv.connect(
+      new Kafka()
+        .version("0.11")
+        .topic("test")
+        .property("zookeeper.connect", "dev41:2181")
+        .property("bootstrap.servers", "dev41:9092")
+    )
+      .withFormat(new Csv)
+      .withSchema(
+        new Schema()
+          .field("id", DataTypes.STRING())
+          .field("temperature", DataTypes.DOUBLE())
+      )
+      .createTemporaryTable("kafkaOutputTable")
+
+//    filterTable.insertInto("kafkaOutputTable")
+    aggTable.insertInto("kafkaOutputTable")
+
+    env.execute()
+  }
+}
