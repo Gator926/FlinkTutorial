e3d1bc542356e8d08638a4e8554bdf57d67786ad
gator <zhangpei529@sohu.com>
2021-08-20 16:41:47 +0800

    commit

---
+++ src/main/scala/com/bigdata/tabletest/TimeAndWindowTest.scala
@@ -0,0 +1,33 @@
+package com.bigdata.tabletest
+
+import com.bigdata.apitest.SensorReading
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.table.api.{EnvironmentSettings, Table}
+import org.apache.flink.table.api.scala._
+import org.apache.flink.types.Row
+
+object TimeAndWindowTest {
+  def main(args: Array[String]): Unit = {
+    val environment = StreamExecutionEnvironment.getExecutionEnvironment
+    environment.setParallelism(1)
+    environment.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
+
+    val settings = EnvironmentSettings.newInstance()
+      .useBlinkPlanner()
+      .inStreamingMode()
+      .build()
+    val tableEnv = StreamTableEnvironment.create(environment, settings)
+
+    val inputStream = environment.readTextFile("src/main/resources/sensor.txt")
+
+    val dataStream = inputStream.map(data => {
+      val arr = data.split(",")
+      SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
+    })
+
+    val sensorTable: Table = tableEnv.fromDataStream(dataStream, 'id, 'temperature, 'timestamp, 'pt.proctime)
+    sensorTable.toRetractStream[Row].print()
+    environment.execute()
+  }
+}
