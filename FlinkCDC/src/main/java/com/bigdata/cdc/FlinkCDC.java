6142411f899897fc6683b81960c17a007cb593b9
gator <zhangpei529@sohu.com>
2021-08-30 11:54:19 +0800

    commit

---
+++ FlinkCDC/src/main/java/com/bigdata/cdc/FlinkCDC.java
@@ -0,0 +1,45 @@
+package com.bigdata.cdc;
+
+import com.ververica.cdc.connectors.mysql.MySqlSource;
+import com.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.ververica.cdc.debezium.DebeziumSourceFunction;
+import com.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class FlinkCDC {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // 开启checkpoint
+//        env.enableCheckpointing(5000);
+//        env.getCheckpointConfig().setAlignmentTimeout(10000);
+//        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+//        env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
+//
+//        env.setStateBackend(new FsStateBackend("file:///C:\\Code\\FlinkTutorial\\FlinkCDC\\src\\main\\checkpoints"));
+
+        // 通过FlinkCDC构建SourceFunction
+        DebeziumSourceFunction<String> sourceFunction = MySqlSource.<String>builder()
+                .hostname("localhost")
+                .serverTimeZone("Asia/Shanghai")
+                .port(3306)
+                .username("root")
+                .password("root")
+                .databaseList("cdc_test")
+//                .tableList("cdc_test.user_info")
+                .deserializer(new StringDebeziumDeserializationSchema())
+                .startupOptions(StartupOptions.initial())
+                .build();
+        DataStreamSource<String> streamSource = env.addSource(sourceFunction);
+
+        // 数据打印
+        streamSource.print();
+
+        // 启动任务
+        env.execute();
+    }
+}
